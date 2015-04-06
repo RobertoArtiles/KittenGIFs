23a95f14d910f92760a3a91709f5a067fd3cecd2
Roberto Artiles Astelarra <me@the-roberto.com>
2015-04-05 17:44:40 -0700

    Add Fabric.

--- app/src/main/java/com/the_roberto/kittengifs/App.java
+++ app/src/main/java/com/the_roberto/kittengifs/App.java
@@ -3,12 +3,15 @@ package com.the_roberto.kittengifs;
 import android.app.Application;
 
 import de.greenrobot.event.EventBus;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         GifsController.init(this, EventBus.getDefault());
         EventsTracker.init(this);
     }

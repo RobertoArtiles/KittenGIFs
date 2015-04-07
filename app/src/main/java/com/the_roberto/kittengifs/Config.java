e22d9e4f9f09ac90f776313401aeb721e3c5d0c0
Roberto Artiles Astelarra <me@the-roberto.com>
2015-04-07 09:43:06 -0700

    Update the API key.

--- app/src/main/java/com/the_roberto/kittengifs/Config.java
+++ app/src/main/java/com/the_roberto/kittengifs/Config.java
@@ -1,5 +1,5 @@
 package com.the_roberto.kittengifs;
 
 public class Config {
-    public static final String GIPHY_API_KEY = "Sm1KTXpsWE9pSTBkcQ==";
+    public static final String GIPHY_API_KEY = "ZGM2emFUT3hGSm16Qw=="; //a public beta key
 }
